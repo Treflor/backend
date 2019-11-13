e89477a96718dedb2d2cc2321b330fba158893ca
Hirantha Rathnayake <sahanhirantha@gmail.com>
2019-11-13 10:59:11 +0530

    google auth response

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -2,7 +2,7 @@ const JWT = require('jsonwebtoken');
 var mimeTypes = require('mimetypes');
 const config = require('../configuration');
 const User = require('../models/user');
-const gcs = require('../helpers/cloud-storage');
+const gcs = require('../cloud-storage');
 
 signToken = (user, method) => {
     return JWT.sign({
