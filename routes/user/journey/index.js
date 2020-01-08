a9d77f759b8d13d3ad9b92e46dd529a1e272c245
hiranthar <sahanhirantha@gmail.com>
2020-01-08 08:43:23 +0530

    missing imports

--- routes/user/journey/index.js
+++ routes/user/journey/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 const journeyController = require('../../../controllers/journey');
 
+const passportJWT = passport.authenticate('jwt', { session: false });
 // const { validateBody, schemas } = require('../../helpers/validators');
 
 // router.route('/edit').post(passportJWT, validateBody(schemas.updateUserSchema), userController.editUser);
